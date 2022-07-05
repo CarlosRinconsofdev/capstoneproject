c7a01e4194401d1824f2ef20ee68981cf50d3ff4
anfepema700 <anfepema700@github.com>
2022-07-05 10:45:43 -0500

    create home.vue

--- public/src/main.js
+++ public/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+const router = createRouter({
+    history: createWebHistory().baseroutes: [{
+        path: '/',
+        component: () =>
+            import ('./components/Home.vue')
+    }]
+
+})
 
 createApp(App).mount('#app')
