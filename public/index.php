a461de0b29746727b0052d20f80d13523f91db0a
anfepema700 <anfepema700@github.com>
2022-07-05 10:39:41 -0500

    update

--- public/index.php
+++ public/index.php
@@ -7,10 +7,10 @@
       rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100&display=swap" rel="stylesheet">
     <title>Capstone project holberton school</title>
-
+npm 
 </head>
   <body>
     <div id="app"></div>
